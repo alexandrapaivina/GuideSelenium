3723c7f4183ff92c9a1519e2ff8eacbe4c8c3c8e
alexandrapaivina@rambler.ru <aSWS3dc123!>
2020-06-29 16:13:34 +0300

    Task 5

--- src/test/java/litecart/WebDriverSettings.java
+++ src/test/java/litecart/WebDriverSettings.java
@@ -2,16 +2,35 @@ package litecart;
 
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WebDriverSettings {
 
-   public ChromeDriver driver;
+   public WebDriver driver;
+   private WebDriverWait wait;
+
 
     @Before
     public void start() {
         System.setProperty("webdriver.chrome.driver","/Users/dev/AuxiliaryExecutables/chromedriver");
-        driver = new ChromeDriver();
+        System.setProperty("webdriver.gecko.driver", "/Users/dev/AuxiliaryExecutables//geckodriver");
+
+        //driver = new ChromeDriver();
+        //driver = new SafariDriver();
+        driver = new FirefoxDriver();
+        wait = new WebDriverWait(driver,10);
+
+       /* DesiredCapabilities caps = new DesiredCapabilities();
+          caps.setCapability("unexpectedAlertBehaviour","dismiss");
+          caps.setCapability("setWindowRect","false");
+          System.out.println(((HasCapabilities) driver).getCapabilities());*/
     }
 
     @After
